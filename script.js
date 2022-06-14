ca8bfd50538b1c7b8ad67b573f70a85d0b99fca1
Amlana-P <amlana.pattnayak@gmail.com>
2022-06-14 22:19:34 +0530

    Modified event type

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function requestOrientationPermission(){
 	DeviceOrientationEvent.requestPermission()
 	.then(response => {
 		if (response == 'granted') {
-			window.addEventListener('deviceorientation', (event) => {
+			window.addEventListener('devicemotion', (event) => {
 				 	xAxis = document.getElementById('xAxis').innerHTML = event.accelerationIncludingGravity.x;
 					yAxis = document.getElementById('yAxis').innerHTML = event.accelerationIncludingGravity.y;
 					zAxis = document.getElementById('zAxis').innerHTML = event.accelerationIncludingGravity.z;
