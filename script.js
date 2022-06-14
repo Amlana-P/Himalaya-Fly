07088bb75319a672a74141981df24d7aff9741f8
Amlana-P <amlana.pattnayak@gmail.com>
2022-06-14 22:13:50 +0530

    Improvement - Requesting permission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function handleMotionEvent(event) {
+// function handleMotionEvent(event) {
 
-    xAxis = document.getElementById('xAxis').innerHTML = event.accelerationIncludingGravity.x;
-	yAxis = document.getElementById('yAxis').innerHTML = event.accelerationIncludingGravity.y;
-	zAxis = document.getElementById('zAxis').innerHTML = event.accelerationIncludingGravity.z;
+//  xAxis = document.getElementById('xAxis').innerHTML = event.accelerationIncludingGravity.x;
+// 	yAxis = document.getElementById('yAxis').innerHTML = event.accelerationIncludingGravity.y;
+// 	zAxis = document.getElementById('zAxis').innerHTML = event.accelerationIncludingGravity.z;
+	
+// 	console.log("Amlana");
 
+// }
+
+// window.addEventListener("devicemotion", handleMotionEvent, true);
+function requestOrientationPermission(){
+	DeviceOrientationEvent.requestPermission()
+	.then(response => {
+		if (response == 'granted') {
+			window.addEventListener('deviceorientation', (event) => {
+				 	xAxis = document.getElementById('xAxis').innerHTML = event.accelerationIncludingGravity.x;
+					yAxis = document.getElementById('yAxis').innerHTML = event.accelerationIncludingGravity.y;
+					zAxis = document.getElementById('zAxis').innerHTML = event.accelerationIncludingGravity.z;
+			})
+		}
+	})
+	.catch(console.error)
 }
-
-window.addEventListener("devicemotion", handleMotionEvent, true);
