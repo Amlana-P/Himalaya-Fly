50cabfbfcc14477187714687f2c04b5e081a5c3c
Amlana-P <amlana.pattnayak@gmail.com>
2022-06-14 22:22:35 +0530

    Testing Script

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function requestOrientationPermission(){
 	.then(response => {
 		if (response == 'granted') {
 			window.addEventListener('devicemotion', (event) => {
-				 	xAxis = document.getElementById('xAxis').innerHTML = event.accelerationIncludingGravity.x;
+				 	// xAxis = document.getElementById('xAxis').innerHTML = event.accelerationIncludingGravity.x;
+					 
+				 	xAxis = document.getElementById('xAxis').innerHTML = "Amlana";
 					yAxis = document.getElementById('yAxis').innerHTML = event.accelerationIncludingGravity.y;
 					zAxis = document.getElementById('zAxis').innerHTML = event.accelerationIncludingGravity.z;
 			})
