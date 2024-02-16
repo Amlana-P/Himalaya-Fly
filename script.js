baec373c55c424a58558c3c57c454822a8648c9b
Amlana <amlana.pattnayak@oracle.com>
2024-02-16 22:18:29 +0530

    removed excess

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 function runGame(){
-
-	if (window.DeviceMotionEvent == undefined) {
-		//No accelerometer is present. 
-		alert("No accelerometer found. You can't play this game.");
-	}
-	else {
-		// alert("Accelerometer found");
-		window.addEventListener("devicemotion", accelerometerUpdate, true);
-	}
+	window.addEventListener("devicemotion", accelerometerUpdate, true);
 
 	function accelerometerUpdate(e) {
-
-		X = Math.round((e.accelerationIncludingGravity.x*100))/100;
-		Y = Math.round((e.accelerationIncludingGravity.y*100))/100;
-		Z = Math.round((e.accelerationIncludingGravity.z*100))/100;
+		console.log(e);
+		X = Math.round(e.accelerationIncludingGravity.x*100)/100;
+		Y = Math.round(e.accelerationIncludingGravity.y*100)/100;
+		Z = Math.round(e.accelerationIncludingGravity.z*100)/100;
 
 		document.getElementById('xAxis').innerHTML = X + ' m/s2';
 		document.getElementById('yAxis').innerHTML = Y + ' m/s2';
@@ -23,12 +15,10 @@ function runGame(){
 		i = 1;
 		addElementFunction = setInterval((x, y, z) => {
 			xyz.push([x, y, z]);
-			// console.log(x, y, z);
 		}, 50, X, Y, Z);
 
 		setTimeout(() => {
 			clearInterval(addElementFunction);
-			// console.log("Stop add element");
 		}, 1000);	
 		
 
@@ -39,35 +29,37 @@ function runGame(){
 		myList.appendChild(listItem);
 		app = document.querySelector('#xyzVals');
 		app.appendChild(myList);
-		// Create an unordered list
-		// var list = document.createElement('ul');
-
-		// Create a list item for each wizard
-		// and append it to the list
-		// xyz.forEach(function (val) {
-		// 	var li = document.createElement('li');
-		// 	li.textContent = val.join();
-		// 	list.appendChild(li);
-		// });
-
-		// var app = document.querySelector('#xyzVals');
-		// app.appendChild(list);
-
-		// console.table(xyz);
 	}
 
 };
 
 async function runTimer(){
 
-	count = 1;
-	const timer = document.getElementById('timer');
+	if (window.DeviceMotionEvent == null) {
+		//No accelerometer is present. 
+		console.log(window.DeviceMotionEvent);
+		alert("No accelerometer found. You can't play this game.");
+	}
 
+	count = -4;
+	const timer = document.getElementById('timer');
 	const updateTimer = setInterval(() =>{
 		timer.innerHTML = count;
-		count--;
+		count++;
 
-		if(count == -1){
+		switch(count){
+			case -3:
+				alert("Get Ready...");
+				break;
+			case -2:
+				alert("Set...");
+				break;
+			case -1:
+				alert("Throw!");
+				break;
+		}
+
+		if(count == 0){
 			clearInterval(updateTimer);
 			timer.innerHTML = "Shoot!";
 			runGame();
